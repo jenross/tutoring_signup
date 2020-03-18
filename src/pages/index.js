d502d0cf02e2269cbe4715c0a1deae26c9279a40
jenross <ross.m.jennifer@gmail.com>
2020-03-18 17:09:31 -0400

    changed header and added headshot

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <Helmet title="Gatsby Starter - Stellar" />
+        <Helmet title="Jennifer Ross Tutoring: All K-6 subject areas and computer programming" />
 
         <Header />
 
