4b83d514fd6ecdcdc59332c853daa69b7ad9528a
jenross <ross.m.jennifer@gmail.com>
2020-03-23 12:16:53 -0400

    calendly links added

--- src/pages/bookme.js
+++ src/pages/bookme.js
@@ -7,7 +7,6 @@ import HeaderGeneric from '../components/HeaderGeneric'
 
 class BookMe extends React.Component {
   render() {
-
     return (
       <Layout>
         <Helmet title="Book Jennifer Ross" />
@@ -16,10 +15,42 @@ class BookMe extends React.Component {
           <section id="content" className="main">
             {/* <span className="image main"><img src={pic04} alt="" /></span> */}
             {/* <h2>Book Me</h2> */}
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <h2>Tempus veroeros</h2>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+            <p>
+              <a
+                href="https://calendly.com/rosstutoring/coding-session"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Coding Session (One Student)
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://calendly.com/rosstutoring/coding-session-2-students"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Coding Session (Two Students)
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://calendly.com/rosstutoring/tutoring-session"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Tutoring (One Student)
+              </a>
+            </p>
+            <p>
+              <a
+                href="https://calendly.com/rosstutoring/guitar-lesson"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Guitar Lesson (One Student)
+              </a>
+            </p>
           </section>
         </div>
       </Layout>
